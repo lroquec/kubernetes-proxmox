bff385b30ae6794ed4cbf848b17d01c57c8e1118
Laura Roque <lroquecu@gmail.com>
2024-11-28 12:48:29 +0100

    variables

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "root_password" {
+  description = "The root password for the VM"
+  type        = string
+  default     = "Enter123!"
+  
+}
+variable "ssh_key" {
+  description = "The SSH key for the VM"
+  type        = string
+  default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE/Pjg7YXZ8Yau9heCc4YWxFlzhThnI+IhUx2hLJRxYE Cloud-Init@Terraform"
+}
