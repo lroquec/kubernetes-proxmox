6ca77e2cc3f83671e08a4ea5e9afe2d9cfdfe4f1
Laura Roque <lroquecu@gmail.com>
2024-11-28 12:59:40 +0100

    add variables

--- compute.tf
+++ compute.tf
@@ -1,12 +1,13 @@
 resource "proxmox_vm_qemu" "cloudinit-example" {
+  count = 3
   vmid        = 100
-  name        = "test-terraform0"
-  target_node = "pve"
+  name        = "kubeadm-vm-${count.index + 1}"
+  target_node = var.proxmox_host
   agent       = 1
   cores       = 2
-  memory      = 1024
+  memory      = 2048
   boot        = "order=scsi0" # has to be the same as the OS disk of the template
-  clone       = "debian12-cloudinit" # The name of the template
+  clone       = var.template # The name of the template
   scsihw      = "virtio-scsi-single"
   vm_state    = "running"
   automatic_reboot = true
@@ -15,7 +16,7 @@ resource "proxmox_vm_qemu" "cloudinit-example" {
   cicustom   = "vendor=local:snippets/kubeadm-cluster.yml" # /var/lib/vz/snippets/kubeadm-cluster.yml
   ciupgrade  = true
   nameserver = "1.1.1.1 8.8.8.8"
-  ipconfig0  = "ip=192.168.1.10/24,gw=192.168.1.1,ip6=dhcp"
+  ipconfig0  = "ip4=dhcp,ip6=dhcp"
   skip_ipv6  = true
   ciuser     = "root"
   cipassword = var.root_password
@@ -33,22 +34,20 @@ resource "proxmox_vm_qemu" "cloudinit-example" {
         disk {
           storage = "local-lvm"
           # The size of the disk should be at least as big as the disk in the template. If it's smaller, the disk will be recreated
-          size    = "2G" 
+          size    = "10G" 
         }
       }
     }
-    ide {
-      # Some images require a cloud-init disk on the IDE controller, others on the SCSI or SATA controller
-      ide1 {
-        cloudinit {
-          storage = "local-lvm"
-        }
-      }
-    }
-  }
+   }
 
   network {
     bridge = "vmbr0"
     model  = "virtio"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      network,
+    ]
   }
 }
