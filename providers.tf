2b8965c4b8cff8d0b28fb8c9af77bef7323acf8a
Laura Roque <lroquecu@gmail.com>
2024-11-30 19:37:12 +0100

    changed because previous to many errors

--- providers.tf
+++ providers.tf
@@ -1,14 +1,19 @@
 terraform {
   required_providers {
     proxmox = {
-      source  = "Telmate/proxmox"
-      version = "2.9.8"
+      source  = "bpg/proxmox"
+      version = "0.68.0"
     }
   }
 }
-
 provider "proxmox" {
-  # Configuration options
-  pm_api_url      = "https://pv02:8006/api2/json"
-  pm_tls_insecure = true
+  endpoint = "https://pv02:8006"
+  insecure = true
+  username = "root@pam"
+  password = var.root_password
+  ssh {
+    agent       = false
+    private_key = file("~/.ssh/id_ed25519")
+    username    = "root"
+  }
 }
