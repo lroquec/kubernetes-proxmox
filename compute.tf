3a8d7f31dbd3122aefeddde7f300d332e1890358
Laura Roque <lroquecu@gmail.com>
2024-11-30 19:37:41 +0100

    create vms from template

--- compute.tf
+++ compute.tf
@@ -1,41 +1,58 @@
-resource "proxmox_vm_qemu" "cloudinit-k8s" {
-  count            = 1
-  vmid             = "40${count.index + 1}"
-  name             = "kubeadm-vm-${count.index + 1}"
-  target_node      = var.proxmox_host
-  agent            = 1
-  cores            = 2
-  memory           = 2048
-  bootdisk         = "scsi0"           # has to be the same as the OS disk of the template
-  clone            = var.template_name # The name of the template
-  scsihw           = "virtio-scsi-pci"
-  automatic_reboot = true
-  os_type = "cloud-init"
+resource "proxmox_virtual_environment_vm" "k8s-servers" {
+  count     = 1
+  vm_id     = "80${count.index + 1}"
+  name      = "kubeadm-vm-${count.index + 1}"
+  node_name = var.proxmox_host
+  tags      = ["terraform", "k8s"]
 
-  # Cloud-Init configuration
-  cicustom   = "vendor=local:snippets/kubeadm-cluster.yml" # /var/lib/vz/snippets/kubeadm-cluster.yml
-  nameserver = "1.1.1.1 8.8.8.8"
-  ipconfig0  = "ip4=dhcp,ip6=dhcp"
-  ciuser     = "root"
-  cipassword = var.root_password
-  sshkeys    = var.ssh_key
-
-  disk {
-    slot     = 0
-    size     = "10G"
-    type     = "scsi"
-    storage  = "local-lvm"
-    iothread = 1
+  agent {
+    enabled = true
   }
 
-  network {
+  clone {
+    vm_id = var.template_id
+    full  = true
+  }
+
+  # keep the first disk as boot disk
+  disk {
+    datastore_id = "local-lvm"
+    interface    = "scsi0"
+    size         = 20
+    file_format  = "raw"
+    cache        = "writeback"
+    iothread     = false
+    ssd          = true
+    discard      = "on"
+  }
+
+  network_device {
     bridge = "vmbr0"
-    model  = "virtio"
+  }
+
+  # Cloud-Init configuration
+  initialization {
+    interface           = "ide2"
+    type                = "nocloud"
+    vendor_data_file_id = "local:snippets/kubeadm-cluster.yml"
+    ip_config {
+      ipv4 {
+        address = "192.168.5.19${count.index + 1}/24"
+        gateway = "192.168.4.1"
+      }
+    }
+
+    dns {
+      servers = ["1.1.1.1"]
+    }
+
+    user_account {
+      keys = [var.ssh_key]
+    }
+
   }
 
   lifecycle {
-    ignore_changes = [
-      network,
-    ]
+    ignore_changes = [initialization["user_account"], ]
   }
 }
